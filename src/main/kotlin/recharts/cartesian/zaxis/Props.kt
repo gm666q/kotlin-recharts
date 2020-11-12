de9f35d7ca39d0a433595f8026c4e5bbe4237076
Jan Śmiałkowski <gm666q@gm666q.space>
2020-11-12 19:27:38 +0000

    Add missing ZAxisProps properties

--- src/main/kotlin/recharts/cartesian/zaxis/Props.kt
+++ src/main/kotlin/recharts/cartesian/zaxis/Props.kt
@@ -1,18 +1,18 @@
 package recharts.cartesian.zaxis
 
 import recharts.kotlin.RechartsProps
+import recharts.kotlin.properties.AxisScaleOrNullProp
 import recharts.kotlin.properties.DataKeyOrNullProp
 import recharts.kotlin.properties.EnumOrNullProp
 import recharts.kotlin.properties.NumberOrStringOrNullProp
 
-// TODO
 external interface Props : RechartsProps {
     var range: Array<Number>?
-    //scale?: ScaleType | Function;
 }
 
 var Props.dataKey by DataKeyOrNullProp<Any>()
 var Props.name by NumberOrStringOrNullProp()
+var Props.scale by AxisScaleOrNullProp()
 var Props.type by EnumOrNullProp(ZAxisType::valueOf)
 var Props.unit by NumberOrStringOrNullProp()
 var Props.zAxisId by NumberOrStringOrNullProp()
